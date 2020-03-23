47626b2060ccac46b894d47ad3a9177c8ff5ff6b
Evelyne Lachance <evelyne.lachance@mindgeek.com>
2020-03-22 22:50:27 -0400

    Implement merge object feature

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import { useState } from 'react';
-import { get, set } from 'lodash';
+import { get, set, merge } from 'lodash';
 
 const useObject = (initialValue = {}) => {
     if(typeof initialValue !== 'Object') {
         throw new Error("Initial value must be of object type");
     }
     const [obj, setObj] = useState(initialValue || {});
-    const setValue = (value, path) => path ? 
-        setObj(set(obj, path, value)) :
-        setObj(value);
+    const setValue = (value, path) => {
+        if(!path) {
+            setObj(value);
+        } else if (path === 'merge') {
+            setObj(merge(obj, value));
+        } else {
+            setObj(set(obj, path, value));
+        }
+    };
+
     const getValue = path => get(obj, path);
     return [obj, setValue, getValue];
 }
